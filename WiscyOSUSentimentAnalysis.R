7d9e61c4afc43baa4cd9795a1691681f6cc3b277
John Haser <32786190+JDHaser@users.noreply.github.com>
2017-12-04 19:20:22 -0500

    Add files via upload

---
+++ WiscyOSUSentimentAnalysis.R
@@ -0,0 +1,40 @@
+library(DataComputing)
+library(RCurl)
+library(tidyr)
+library(lubridate)
+
+x <- getURL("https://raw.githubusercontent.com/JDHaser/DS220-Final-Project/master/OSUWiscyTweets.csv")
+RawTweetData <- read.csv(text = x)
+
+ex <- "^([0-9]{4}[-][0-9]{2}[-][0-9]{2}[ ][0-9]{2}[:][0-9]{2}[:][0-9]{2})"
+
+TimedTweets <- RawTweetData %>% filter(grepl(ex, Tweets))
+
+ValuedTweets <- TimedTweets %>% mutate(Value = ifelse(Level.1.label == "Neutral", 0, ifelse(Level.1.label == "Negative", -1, 1)))
+
+WeightedTweets <- ValuedTweets %>% mutate(weightedValue = Value * Level.1.probability) %>% select("Tweets", "Value", "weightedValue")
+
+TimeStampedTweets <- WeightedTweets %>% mutate(Time = substr(Tweets, 12,19))
+
+DayTweets <- TimeStampedTweets %>% mutate(Day = substr(Tweets, 0, 10))
+
+SummarisedTweets <- DayTweets %>% mutate(Time = ifelse(Day == "2017-12-02",  (as.numeric(hms(Time)) / 3600), (as.numeric(hms(Time)) / 3600) + 24))
+
+SummarisedTweets <- SummarisedTweets %>% group_by(Time) %>% summarise(Sentiment = mean(weightedValue))
+
+ggplot(SummarisedTweets, aes(x = Time, y = Sentiment)) + geom_line() +
+  labs(title = "Sentiment Analysis of the Ohio State Wisconsin Game", x = "Time of Day(Hours)", y = "Average Sentiment") +
+  expand_limits(y = c(-.8, .8)) +
+  scale_x_continuous(breaks = seq(0, 28, by = 4)) +
+  theme_minimal() +
+  geom_hline(yintercept = 0, color = "red") +
+  geom_vline(xintercept = 20, color = "green") +
+  geom_vline(xintercept = 24.5, color = "green")
+
+GameTimeTweets <- SummarisedTweets %>% filter(Time >= 20 & Time <= 24.5)
+
+ggplot(GameTimeTweets, aes(x = Time, y = Sentiment)) + geom_line() +
+  labs(title = "Sentiment Analysis of the Ohio State Wisconsin Game", x = "Time of Day(Hours)", y = "Average Sentiment") +
+  expand_limits(y = c(-.8, .8)) +
+  theme_minimal() +
+  geom_hline(yintercept = 0, color = "red")
